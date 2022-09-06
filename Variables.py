a2aa005f7770fae4d46742352eac0003544ab0bd
LeoPa1305 <111544910+LeoPa1305@users.noreply.github.com>
2022-09-05 23:13:46 -0500

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,24 @@
+
+
+mensaje = "bienvenido a mi repositorio de Python" 
+print(mensaje)
+
+x = 13
+y = 5
+
+z = x+y #Una simple suma con una nueva variable
+
+print("El resultado de la suma es: ",z) #Se imprime la variable
+
+a = 13.73
+b = 5.2745555
+
+b= round(b,2) #Se utiliza la función redondear, para que solo queden dos decimales
+
+c = a+b
+
+print("El resultado de la suma es: ",c)
+
+
+
+
